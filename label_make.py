f81bb495576691c1c08228fee44af682d3ce3b6f
flyingtom <cys.tom@outlook.com>
2021-12-07 02:23:15 +0800

    update

--- label_make.py
+++ label_make.py
@@ -1,9 +1,11 @@
 import numpy as np
 from algorithms.SVM import SVM
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import SMOTE
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
 
@@ -21,18 +23,20 @@ def get_data(train=True):
 
 
 ros = RandomOverSampler(random_state=0)
+smo = SMOTE(random_state=42)
 
 
-X_train, y_train = get_data(train=True)
-# X_train, y_train = ros.fit_resample(X_train, y_train)
-X_test = get_data(train=False)
+# X_train, y_train = smo.fit_sample(X, y)
+X, y = get_data(train=True)
+X_train, y_train = smo.fit_resample(X, y)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=123)
+X_tocal = get_data(train=False)
 
 
-clf = make_pipeline(StandardScaler(), SVC(C=0.8, kernel='rbf', decision_function_shape='ovr', class_weight='balanced'))
-# clf = SVC(gamma="auto")
-# clf = SVR(kernel="poly",  gamma="auto", degree=3, epsilon=0.1, coef0=1)
+#clf = make_pipeline(StandardScaler(), SVC(C=0.5, kernel='rbf',  class_weight='balanced'))
+clf = SVC(C=0.4, kernel='rbf',  class_weight='balanced')
 clf.fit(X_train, y_train)
 
-y_pred = clf.predict(X_train)
-print(f"Train Error|| Accuracy: {100*metrics.accuracy_score(y_train, y_pred):>0.1f}% \n")
-np.savetxt('output_191250004.txt', clf.predict(X_test), fmt='%d')
+# y_pred = clf.predict(X_test)
+# print(f"Train Error|| Accuracy: {100*metrics.accuracy_score(y_test, y_pred):>0.1f}% \n")
+np.savetxt('output_191250004.txt', clf.predict(X_tocal), fmt='%d')
---
+++ nn.py
@@ -0,0 +1,105 @@
+import torch
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.optim.lr_scheduler import LambdaLR
+import time
+import torch
+from torch.utils.data import Dataset, DataLoader
+import logging
+from data_utils import get_data
+from imblearn.over_sampling import RandomOverSampler
+ros = RandomOverSampler(random_state=0)
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+device = 'cpu'
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+logger.info('Using {} device'.format(device))
+
+
+class Dataset(Dataset):
+    def __init__(self, train=True, corrupt=False):
+        features, labels = get_data(train, corrupt)
+        # if corrupt == True:
+        #     features, labels = ros.fit_resample(features, labels)
+        self.features = torch.from_numpy(features).float().to(device)
+        self.labels = torch.from_numpy(labels).to(device)
+
+    def __getitem__(self, index):
+        return self.features[index], self.labels[index]
+
+    def __len__(self):
+        return len(self.features)
+
+
+train_dataloader = DataLoader(Dataset(train=True, corrupt=False), batch_size=32, shuffle=True)
+test_dataloader = DataLoader(Dataset(train=False, corrupt=False), batch_size=32, shuffle=False)
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(16, 128),
+            nn.Linear(128, 10),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.linear_relu_stack(x)
+
+
+model = NeuralNetwork().to(device)
+
+
+def train_loop():
+    for X, y in train_dataloader:
+        pred = model(X)
+        loss = criterion(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # scheduler.step()
+
+    # if batch % 100 == 0:
+    #     loss, current = loss.item(), batch * len(X)
+    #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop():
+    size = len(test_dataloader.dataset)
+    num_batches = len(test_dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in test_dataloader:
+            pred = model(X)
+            test_loss += criterion(pred, y).item()
+            correct += (pred.argmax(dim=1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error|| Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+
+
+criterion = nn.CrossEntropyLoss()
+#optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.5)
+# optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, weight_decay=1e-5)
+# scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: 1/(epoch/10+11))
+
+for epoch in range(30):
+    train_loop()
+    # if(epoch % 20 == 0):
+    print(f"Epoch {epoch+1}\n-------------------------------")
+    #print(f"lr: {optimizer.param_groups[0]['lr']}")
+    test_loop()
+
+
+# curtime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+# torch.save(model, f"./models/epo_{curtime}")
+# print("Done!")
